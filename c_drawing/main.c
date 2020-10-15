ef09c5cedc171af253433cbcdf59118b4274ae39
pworontzoff <pierre.worontzoff@hepl.be>
2020-10-15 22:15:59 +0200

    Update main.c

--- c_drawing/main.c
+++ c_drawing/main.c
@@ -2,14 +2,8 @@
 #include <stdlib.h>
 #include "drawing.h"
 
-#define WIDTH 400
-#define HEIGHT 400
-
 int main() {
-    init_drawing(WIDTH, HEIGHT, 0.5);
-    
     
 
-    display_drawing();
     return 0;
 }
